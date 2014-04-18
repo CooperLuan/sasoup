748e4de362de96689895da2ad0d332fe7c5f119d
Cooper.luan <gc.suprs@gmail.com>
2014-04-18 17:55:05 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,24 @@
 # coding: utf-8
 from setuptools import setup, find_packages
 
-AUTHOR = 'Cooper'
-AUTHOR_EMAIL = 'lannikcooper@gmail.com'
+AUTHOR = 'Cooper.luan'
+AUTHOR_EMAIL = 'gc.suprs@gmail.com'
 PACKAGE = 'sasoup'
-NAME = 'sasoup'
-DESCRIPTION = 'sasoup'
 VERSION = __import__(PACKAGE).__version__
 
 
-setup(name='sasoup',
-      version=VERSION,
-      description='html parser base on rules',
-      long_description="""\
-ryan""",
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-      ],
-      keywords='sasoup',
-      author=AUTHOR,
-      author_email=AUTHOR_EMAIL,
-      url=URL,
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['sasoup'],
-      include_package_data=True,
-      test_suite='nose.collector',
-      test_requires=['Nose'],
-      zip_safe=True,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+setup(
+    name='sasoup',
+    version=VERSION,
+    description='html parser base on rules',
+    keywords='sasoup',
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url='https://github.com/lannikcooper/sasoup',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'pyyaml',
+        'lxml',
+    ]
+)
